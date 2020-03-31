1b2f6e702e8dd05985405ac5831a2b499376846d
MattIPv4 <me@mattcowley.co.uk>
2020-03-31 13:42:03 +0100

    Add support for adding git auto-update

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const fs = require('fs').promises;
 const readline = require('readline');
 
 const fetch = require('node-fetch');
-const download = require('download-package-tarball');
+const download = require('download-tarball');
 const glob = require('glob').sync;
 const chalk = require('chalk');
 const config = require('./config');
 const { Octokit } = require('@octokit/rest');
 const octokit = new (Octokit.plugin(require('octokit-create-pull-request')))({ auth: config.githubToken });
+const gitUrlParse = require('git-url-parse');
 
 /**
  * Ask the user a question via console and wait for the response
@@ -130,60 +131,39 @@ const createPR = async data => {
     });
 };
 
-const main = async () => {
-    const [, , rawName] = process.argv;
+/**
+ * Allows the user to select either NPM or GitHub for auto-updating
+ *
+ * @returns {Promise<Number>} The selection made by the user (1: NPM, 2: GitHub)
+ */
+const updateChoice = async () => {
+    while (true) {
+        try {
+            const choice = await input(chalk.cyan.bold('1) Auto-update via NPM package\n2) Auto-update via tagged GitHub repo\nAuto-update method to use (1 or 2): '));
+            const cleaned = Number(choice.toString().trim());
 
-    // Validate
-    if (!rawName) {
-        console.error(chalk.red('Usage: node index.js <cdnjsLibraryName>'));
-        return;
+            if ([1, 2].includes(cleaned)) {
+                return cleaned;
+            }
+
+            console.error(chalk.red('Please select either \'1\' or \'2\' for auto-update.'));
+        } catch (_) {
+        }
     }
+};
 
-    // Build the base package
-    const cdnjsData = {};
-    cdnjsData.name = rawName;
-
-    // Get the NPM package name to use
-    const npmName = (await input(chalk.cyan.bold(`NPM package name (blank for ${rawName}): `))).trim() || rawName;
-
-    // Get the package from NPM
-    const rawData = await fetch(`https://registry.npmjs.com/${npmName}`);
-    const jsonData = await rawData.json();
-
-    // Error if NPM errored
-    if (jsonData.error) {
-        console.error(chalk.red(jsonData.error));
-        return;
-    }
-
-    cdnjsData.description = jsonData.description || '';
-    cdnjsData.keywords = jsonData.keywords || [];
-    cdnjsData.author = jsonData.author || '';
-    cdnjsData.license = jsonData.license || '';
-    cdnjsData.homepage = jsonData.homepage || '';
-    cdnjsData.repository = jsonData.repository || {};
-
-    // Get the latest version from NPM
-    const jsonVersionData = jsonData.versions[jsonData['dist-tags'].latest];
-
-    // Ack
-    console.log(`Located ${jsonData.name}@${jsonData['dist-tags'].latest}...`);
-
-    // Download tarball
-    const tarPath = join(__dirname, 'temp', jsonData.name, jsonData['dist-tags'].latest);
-    await download({
-        url: jsonVersionData.dist.tarball,
-        dir: tarPath,
-    });
-
-    // Ack
-    console.log(`Downloaded ${jsonData.name}@${jsonData['dist-tags'].latest}...\n`);
-
+/**
+ * Allows the user to first explore the library before providing the auto-update configuration.
+ *
+ * @param {String} path The base path to explore from
+ * @returns {Promise<fileMap>} The auto-update fileMap provided by the user
+ */
+const exploreAndGlob = async path => {
     // Allow the user to explore
-    await explore(join(tarPath, jsonData.name));
+    await explore(path);
 
     // Allow the user to test globs
-    await globExplore(join(tarPath, jsonData.name));
+    await globExplore(path);
 
     // Get final auto-update
     const fileMap = [];
@@ -225,20 +205,218 @@ const main = async () => {
         }
     }
 
-    // Store the auto-update config
+    return fileMap;
+};
+
+/**
+ * List all files in the version given by the path that match the file map, let the user pick one or none
+ *
+ * @param {String} path The base path to run the fileMap in
+ * @param {fileMap} fileMap The list of file map items to run
+ * @param {String} version The version this path is for
+ * @returns {Promise<String>} The file the user selected, or an empty string
+ */
+const chooseDefault = async (path, fileMap, version) => {
+    const allFiles = allFileMapFiles(path, fileMap);
+    console.log(`\nFiles matching file map in ${version}:\n${allFiles.join('\t')}`);
+    return await input(chalk.cyan.bold(`Default file to highlight for usage (blank to skip): `));
+};
+
+/**
+ * Generate the full cdnjsData for an NPM package
+ *
+ * @param {Object} cdnjsData The initial, bare-bones cdnjsData
+ * @returns {Promise<{Object}>|undefined} The fully-fledged cdnjsData for the NPM package
+ */
+const npm = async cdnjsData => {
+    // Get the NPM package name to use
+    const npmName = (await input(chalk.cyan.bold(`\nNPM package name (blank for ${cdnjsData.name}): `))).trim()
+        || cdnjsData.name;
+
+    // Get the package from NPM
+    const rawData = await fetch(`https://registry.npmjs.com/${npmName}`);
+    const jsonData = await rawData.json();
+
+    // Error if NPM errored
+    if (jsonData.error) {
+        console.error(chalk.red(jsonData.error));
+        return;
+    }
+
+    // Store basic info
+    cdnjsData.description = jsonData.description || '';
+    cdnjsData.keywords = jsonData.keywords || [];
+    cdnjsData.author = jsonData.author || '';
+    cdnjsData.license = jsonData.license || '';
+    cdnjsData.homepage = jsonData.homepage || '';
+    cdnjsData.repository = jsonData.repository || {};
+
+    // Get the latest version from NPM
+    const jsonVersionData = jsonData.versions[jsonData['dist-tags'].latest];
+
+    // Ack
+    console.log(`Located ${jsonData.name}@${jsonData['dist-tags'].latest}...`);
+
+    // Download tarball
+    const tarPath = join(__dirname, 'temp', jsonData.name, jsonData['dist-tags'].latest);
+    await download({
+        url: jsonVersionData.dist.tarball,
+        dir: tarPath,
+    });
+
+    // Ack
+    console.log(`Downloaded ${jsonData.name}@${jsonData['dist-tags'].latest}...\n`);
+
+    // Let the user explore and provide the auto-update config
     cdnjsData.npmName = jsonData.name;
-    cdnjsData.npmFileMap = fileMap;
+    cdnjsData.npmFileMap = await exploreAndGlob(join(tarPath, jsonData.name));
 
     // Get the default filename
-    const allFiles = allFileMapFiles(join(tarPath, jsonData.name), fileMap);
-    console.log(`\nFiles matching file map in ${jsonData['dist-tags'].latest}:\n${allFiles.join('\t')}`);
-    const filename = await input(chalk.cyan.bold(`Default file to highlight for usage (blank to skip): `));
-    if (filename) {
-        cdnjsData.filename = filename;
+    const defaultFile = chooseDefault(join(tarPath, jsonData.name), cdnjsData.npmFileMap, jsonData['dist-tags'].latest);
+    if (defaultFile) {
+        cdnjsData.filename = defaultFile;
     }
 
     // Cleanup
     await fs.rmdir(tarPath, { recursive: true });
+
+    return cdnjsData;
+};
+
+/**
+ * Prompts the user to provide a GitHub.com repository, will loop until they provide a valid repo
+ *
+ * @returns {Promise<[String, String]>} The owner and name of the repository on GitHub.com
+ */
+const githubRepo = async () => {
+    while (true) {
+        try {
+            const repoInput = await input(chalk.cyan.bold('\nGitHub repository URL: '));
+            const parsedRepo = gitUrlParse(repoInput);
+
+            if (parsedRepo.source === 'github.com' && parsedRepo.owner && parsedRepo.name) {
+                return [parsedRepo.owner, parsedRepo.name];
+            }
+
+            console.error(chalk.red('Could not parse a valid GitHub.com repo from given input.'));
+        } catch (_) {
+        }
+    }
+};
+
+/**
+ * Generate the full cdnjsData for a GitHub.com repository
+ *
+ * @param {Object} cdnjsData The initial, bare-bones cdnjsData
+ * @returns {Promise<{Object}>|undefined} The fully-fledged cdnjsData for the GitHub.com repository
+ */
+const github = async cdnjsData => {
+    // Get the git repo name & owner
+    const [repoOwner, repoName] = await githubRepo();
+
+    // Get the tags from the repo
+    const repoTags = await octokit.repos.listTags({
+        owner: repoOwner,
+        repo: repoName,
+    });
+
+    // Error if no tags found
+    if (!repoTags.data || !repoTags.data.length) {
+        console.error(chalk.red(`No tags found in repository https://github.com/${repoOwner}/${repoName}`));
+        return;
+    }
+
+    // Store basic info
+    cdnjsData.description = ''; // Get repo desc?
+    cdnjsData.keywords = []; // Get repo topics?
+    cdnjsData.author = repoOwner;
+    cdnjsData.license = ''; // Get repo license?
+    cdnjsData.homepage = `https://github.com/${repoOwner}/${repoName}`; // Get repo website?
+    cdnjsData.repository = {
+        type: 'git',
+        url: `git://github.com/${repoOwner}/${repoName}.git`,
+    };
+
+    // Ack
+    console.log(`Located ${repoOwner}/${repoName}@${repoTags.data[0].name}...`);
+
+    // Download tarball
+    const tarPath = join(__dirname, 'temp', repoOwner, repoName, repoTags.data[0].name);
+    await download({
+        url: repoTags.data[0].tarball_url,
+        dir: tarPath,
+    });
+    const fullPath = join(tarPath, (await fs.readdir(tarPath))[0]);
+
+    // Ack
+    console.log(`Downloaded ${repoOwner}/${repoName}@${repoTags.data[0].name}...\n`);
+
+    // Use package.json data if we can
+    let jsonFile;
+    try {
+        const rawFile = await fs.readFile(join(fullPath, 'package.json'));
+        jsonFile = JSON.parse(rawFile.toString('utf8'));
+    } catch (_) {
+        try {
+            const rawFile = await fs.readFile(join(fullPath, 'bower.json'));
+            jsonFile = JSON.parse(rawFile.toString('utf8'));
+        } catch (_) {
+        }
+    }
+
+    if (jsonFile) {
+        cdnjsData.description = jsonFile.description || cdnjsData.description;
+        cdnjsData.keywords = jsonFile.keywords || cdnjsData.keywords;
+        cdnjsData.author = jsonFile.author || cdnjsData.author;
+        cdnjsData.license = jsonFile.license || cdnjsData.license;
+        cdnjsData.homepage = jsonFile.homepage || cdnjsData.homepage;
+    }
+
+    // Let the user explore and provide the auto-update config
+    cdnjsData.autoupdate = {
+        source: 'git',
+        target: `git://github.com/${repoOwner}/${repoName}.git`,
+        fileMap: await exploreAndGlob(fullPath),
+    };
+
+    // Get the default filename
+    const defaultFile = chooseDefault(fullPath, cdnjsData.autoupdate.fileMap, repoTags.data[0].name);
+    if (defaultFile) {
+        cdnjsData.filename = defaultFile;
+    }
+
+    // Cleanup
+    await fs.rmdir(tarPath, { recursive: true });
+
+    return cdnjsData;
+};
+
+const main = async () => {
+    const [, , rawName] = process.argv;
+
+    // Validate
+    if (!rawName) {
+        console.error(chalk.red('Usage: node index.js <cdnjsLibraryName>'));
+        return;
+    }
+
+    // Build the base package
+    let cdnjsData = {};
+    cdnjsData.name = rawName;
+
+    // Get auto-update method
+    const updateMethod = await updateChoice();
+    switch (updateMethod) {
+        case 1:
+            cdnjsData = await npm(cdnjsData);
+            break;
+
+        case 2:
+            cdnjsData = await github(cdnjsData);
+            break;
+    }
+
+    if (!cdnjsData) return;
 
     // Done
     //try {
